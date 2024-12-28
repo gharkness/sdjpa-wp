7b95c9134a8ec4d4e365d00ee623e7a1fe96a3dc
Garrett Harkness <gharkness@sourceallies.com>
2024-12-28 16:19:13 -0600

    end of section 21

---
+++ src/main/java/com/gharkness/sdjpawp/domain/CommentMeta.java
@@ -0,0 +1,31 @@
+package com.gharkness.sdjpawp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "wp_commentmeta", indexes = {
+        @Index(name = "comment_id", columnList = "comment_id"),
+        @Index(name = "meta_key", columnList = "meta_key")
+})
+public class CommentMeta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meta_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Size(max = 255)
+    @Column(name = "meta_key")
+    private String metaKey;
+
+    @Lob
+    private String metaValue;
+}
